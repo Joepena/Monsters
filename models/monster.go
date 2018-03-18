86d0ac70a241a58dc91428e4cc52412487a7fae2
Jacob Ville <VilleLJacob@gmail.com>
2018-03-18 18:38:45 -0400

    Add fields to monster stats

--- models/monster.go
+++ models/monster.go
@@ -3,22 +3,25 @@ package models
 import "github.com/pkg/errors"
 
 type Monster struct {
-	ID		string   `bson:"id" json:"monsterID"` //not _id, set when added to a user
-	No      int32    `bson:"no" json:"monsterNo"`
-	Name    string   `bson:"name" json:"name"`
-	Type    string   `bson:"type" json:"type"`
-	Hp      int32    `bson:"hp" json:"hp"`
-	Attack  int32    `bson:"attack" json:"attack"`
+	ID		string   `bson:"id"      json:"monsterID"` //not _id, set when added to a user
+	No      int32    `bson:"no"      json:"monsterNo"`
+	Name    string   `bson:"name"    json:"name"`
+	Type    string   `bson:"type"    json:"type"`
+	Hp      int32    `bson:"hp"      json:"hp"`
+	Attack  int32    `bson:"attack"  json:"attack"`
 	Defense int32    `bson:"defense" json:"defense"`
 	Attacks []Attack `bson:"attacks"`
 	Stats            `bson:",inline"`
 }
 
 type Stats struct {
-	Hits           int32 `bson:"hits" json:"hits"`
-	Misses         int32 `bson:"misses" json:"misses"`
-	DamageDealt    int32 `bson:"damage_dealt" json:"damageDealt"`
-	DamageReceived int32 `bson:"damage_received" json:"damageReceived"`
+	Hits            int32 `bson:"hits"             json:"hits"`
+	Misses          int32 `bson:"misses"           json:"misses"`
+	DamageDealt     int32 `bson:"damage_dealt"     json:"damageDealt"`
+	DamageReceived  int32 `bson:"damage_received"  json:"damageReceived"`
+	EnemiesFought   int32 `bson:"enemies_fought"   json:"enemiesFought"`
+	EnemiesDefeated int32 `bson:"enemies_defeated" json:"enemiesDefeated"`
+	Faints			int32 `bson:"faints"           json:"faints"`
 }
 
 func (m *Monster) Create() error {
